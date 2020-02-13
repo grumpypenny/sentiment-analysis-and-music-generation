3b040593a959d36c6fe6b2a4b6f6192af455c52a
sharvenp <sharven.dhanasekar@mail.utoronto.ca>
2020-02-13 16:28:50 -0500

    Implement data clean without duplication

--- Python Files/data_cleaner.py
+++ Python Files/data_cleaner.py
@@ -1,6 +1,16 @@
 
 import csv
 import re
+import sys
+
+DATA_BUNDLER = {"surprise" : "excited", "enthusiasm" : "excited", "fun":"excited",
+                "hate" : "anger", "anger" : "anger", 
+                "worry" : "worry",
+                "sadness" : "sad", "empty" : "sad",
+                "relief" : "relief",
+                "love" : "love",
+                "happiness" : "happy",
+                "neutral" : "neutral"}
 
 class DataCleaner:
 
@@ -8,39 +18,58 @@ class DataCleaner:
     def generate_clean_dataset(path_load, path_save):
         
         data = [] 
-        tweet_acc = {}
+        emotions = {}
 
         with open(path_load, mode='r') as csv_data:
 
             csv_reader = csv.reader(csv_data, delimiter=',')
             line_count = 0
+    
             for row in csv_reader:
+    
+                if line_count == 0:
+                    line_count += 1
+                    continue
+    
                 emotion = row[1]
                 tweet = row[3]
 
                 if ("\t" in tweet or " " in tweet): # Remove tabs and spaces and replace with one space
                     tweet = re.sub(r'[\t\s]+', ' ', tweet)
-
-                if (row[2] in tweet_acc):
-                    print(f"DUPLICATE USER {row[2]}")
-                    print(f"TWEET 1: {tweet_acc[row[2]]}")
-                    print(f"TWEET 2: {tweet}\n")
-
-                tweet_acc[row[2]] = tweet
-
-                # Strip @person from tweets
                 
+                # Strip @person from tweets
+                if '@' in tweet:
+                    # print(f"OLD: {tweet}")
+                    tweet = re.sub(r'@[^\s]+(\s?)', "", tweet)
+                    if not tweet:
+                        continue
+                    # print(f"NEW: {tweet}\n")
 
+                ## Make All Lowercase? ##
 
-                data.append((emotion, tweet))
+                if emotion != "sentiment" and emotion != "boredom":
+                    if DATA_BUNDLER[emotion] not in emotions:
+                        emotions[DATA_BUNDLER[emotion]] = 0
+                    emotions[DATA_BUNDLER[emotion]] += 1
+                    data.append([DATA_BUNDLER[emotion], tweet])
+
                 line_count += 1
 
+            print(emotions, len(emotions))
+            print(f"Processed {line_count} data points.")
 
 
+        with open(sys.argv[1]+".csv", "w+", newline='') as new_data:
+
+            writer = csv.writer(new_data)
+            writer.writerow(["emotion", "tweet"])
+
+            for data_point in data:
+                writer.writerow(data_point)
 
 
-            print(f"Generated Dataset, Processed {line_count} data points.")
-            # print(data)
+            print(f"Generated Dataset.")
+
 
 
 if __name__ == '__main__':
